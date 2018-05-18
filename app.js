5c22ffefa68d7a965efb890d3ecfeb255b64a45b
bambang s <bambang_supriadi@programmer.net>
2018-05-18 13:49:22 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var multer  = require('multer')
+var storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, './uploads/');  
+  },
+  filename: function(req, file, cb)
+  {
+    cb(null, file.originalname);
+  }
+  
+});
+
+var upload = multer({ storage: storage});
+
+var app = express()
+
+app.post('/single', upload.single('photo'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log(req.body)
+  console.log(req.file)
+  res.status(201).send('success')
+})
+
+app.post('/array', upload.array('photos', 12), function (req, res, next) {
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+  console.log(req.body)
+  console.log(req.files)
+  res.status(201).send('success')
+})
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+})
